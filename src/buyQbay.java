b47f66fa5d79964f4ea3185d26c15d0f3736f556
hcort26 <144395316+hcort26@users.noreply.github.com>
2025-03-26 19:26:51 -0400

    add to cart function started

--- src/buyQbay.java
+++ src/buyQbay.java
@@ -11,59 +11,83 @@ public class buyQbay {
         products[3] = "Leather Couch";
         products[4] = "Quinnipiac Sweatshirt";
 
-        System.out.println("These are the following items for sale:");
+        boolean shopping = true;
 
-        for(int i = 0; i < products.length; i++) {
+        while(shopping) {
 
-            System.out.println("Product at index " + i + ": " + products[0]);
+            System.out.println("These are the following items for sale:");
 
+            for(int i = 0; i < products.length; i++) {
+
+                System.out.println("Product at index " + (i + 1) + ": " + products[i]);
+
+            }
+
+            System.out.println("Enter the number of the index for more product information.");
+            int productid = scanner.nextInt();
+            scanner.nextLine(); // consume leftover newline
+
+            switch(productid) {
+                case 1: 
+                System.out.println("Product Name: HS101 Textbook");
+                System.out.println("Category: Textbook");
+                System.out.println("Seller: John Doe");
+                System.out.println("Email: johndoe@quinnipiac.edu");
+                System.out.println("Price: 25$");
+                break;
+
+                case 2: 
+                System.out.println("Product Name: Wooden Desk");
+                System.out.println("Category: Furniture");
+                System.out.println("Seller: Jane Doe");
+                System.out.println("Email: janedoe@quinnipiac.edu");
+                System.out.println("Price: 75$");
+                break;
+
+                case 3: 
+                System.out.println("Product Name: MA101 Textbook");
+                System.out.println("Category: Textbook");
+                System.out.println("Seller: Noel Kirby");
+                System.out.println("Email: noelkirby@quinnipiac.edu");
+                System.out.println("Price: 30$");
+                break;
+
+                case 4: 
+                System.out.println("Product Name: Leather Couch");
+                System.out.println("Category: Furniture");
+                System.out.println("Seller: Emre Barrera");
+                System.out.println("Email: emrebarrera@quinnipiac.edu");
+                System.out.println("Price: 250$");
+                break;
+
+                case 5: 
+                System.out.println("Product Name: Quinnipiac Sweatshirt");
+                System.out.println("Category: Clothing");
+                System.out.println("Seller: Jake Schultz");
+                System.out.println("Email: jakeschultz@quinnipiac.edu");
+                System.out.println("Price: 40$");
+                break;
+
+                default:
+                    System.out.println("Invalid index. Try again.");
+                    continue; // skip back to start of loop
+            }
+
+            System.out.println("Would you like to add this product to cart? Enter Y/N");
+
+            String addtoCart = scanner.nextLine();
+
+            if(addtoCart.equalsIgnoreCase("Y")) {
+                System.out.println("Product added to cart.");
+                shopping = false; // end loop if they add to cart
+            } else if(addtoCart.equalsIgnoreCase("N")) {
+                System.out.println("Returning to product list...");
+            } else{
+                System.out.println("Incorrect letter, please re-enter your choice.");
+            }
         }
-        System.out.println("Enter the number of the index for more product information.");
 
-        int productid = scanner.nextInt();
-
-        switch(productid) {
-            case 1: 
-            System.out.println("Product Name: HS101 Textbook");
-            System.out.println("Category: Textbook");
-            System.out.println("Seller: John Doe");
-            System.out.println("Email: johndoe@quinnipiac.edu");
-            System.out.println("Price: 25$");
-            break;
-
-            case 2: 
-            System.out.println("Product Name: Wooden Desk");
-            System.out.println("Category: Furniture");
-            System.out.println("Seller: Jane Doe");
-            System.out.println("Email: janedoe@quinnipiac.edu");
-            System.out.println("Price: 75$");
-            break;
-
-            case 3: 
-            System.out.println("Product Name: MA101 Textbook");
-            System.out.println("Category: Textbook");
-            System.out.println("Seller: Noel Kirby");
-            System.out.println("Email: noelkirby@quinnipiac.edu");
-            System.out.println("Price: 30$");
-            break;
-
-            case 4: 
-            System.out.println("Product Name: Leather Couch");
-            System.out.println("Category: Furniture");
-            System.out.println("Seller: Emre Barrera");
-            System.out.println("Email: emrebarrera@quinnipiac.edu");
-            System.out.println("Price: 250$");
-            break;
-
-            case 5: 
-            System.out.println("Product Name: Quinnipiac Sweatshirt");
-            System.out.println("Category: Clothing");
-            System.out.println("Seller: Jake Schultz");
-            System.out.println("Email: jakeschultz@quinnipiac.edu");
-            System.out.println("Price: 40$");
-            break;
-        }
-
+        scanner.close();
     }
 }
 
